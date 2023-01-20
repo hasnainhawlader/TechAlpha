db004ee1cfb61669b7cbcfe5440badfafce46338
hasnainhawlader <hasnain.hawlader@gmail.com>
2023-01-21 00:38:43 +0600

    redux

--- src/app/store.js
+++ src/app/store.js
@@ -3,9 +3,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import productsReducer, {
   productsFetching,
 } from "../feature/products/productSlice";
+
 export const store = configureStore({
   reducer: {
     products: productsReducer,
   },
 });
+
 store.dispatch(productsFetching());
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,16 +1,17 @@
-import React from "react";
 import { useSelector } from "react-redux";
 import Card from "../components/Card";
 
 const Products = () => {
-  const { items: data, status } = useSelector((state) => state.Products);
+  const { items: data, status } = useSelector((state) => state.products);
+
   return (
-    <div className="product section container mx-auto py-10">
-      <h2 className="section-title uppercase text-2xl font-bold space-font text-center">
+    <div className="products-section container mx-auto py-10">
+      <h2 className="section-title uppercase text-2xl font-bold space-font text-center mb-10">
         Browse all products
       </h2>
-      <div className="products-wrapper  grid gird-clos-1 md:grid-cols-2 lg:grid-cols-4 justify-center items-center justify-items-center gap-10">
-        {status && <p>{status}</p>}
+      <div className="products-wrapper grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-10">
+        {status && <p className="col-span-full text-center">{status}</p>}
+
         {data.map((product) => (
           <Card key={product.id} product={product} />
         ))}
